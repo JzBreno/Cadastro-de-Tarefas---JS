06a217f9666cd3d22ba7f7f9a606b3a0f5e3b1e9
JzBreno <jose.breno.sousa61@aluno.ifce.edu.br>
2024-07-08 00:01:48 -0300

    update delete and cadastro

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function novaTarefa(){
+function abrirModal(){
     overlay.classList.add("active");
     criarTarefa.classList.add("active");
 }
@@ -7,3 +7,79 @@ function fecharModal(){
     overlay.classList.remove("active");
     criarTarefa.classList.remove("active");
 }
+
+function buscarTarefas(){
+    fetch("http://localhost:3000/tarefas")
+    .then(res => res.json())
+    .then(res => {
+        inserirTarefas(res);
+    })
+} 
+
+buscarTarefas();
+
+function inserirTarefas(listadeTarefas){
+    if(listadeTarefas.length > 0){
+        lista.innerHTML = ""
+        listadeTarefas.map(tarefa => {
+            lista.innerHTML += ` 
+                <li>
+                    <h5>${tarefa.titulo}</h5>
+                    <p>${tarefa.descricao}</p>
+                    <div class="actions">
+                        <box-icon name='trash' size="sm" onclick="deletarTarefa(${tarefa.id})"></box-icon>
+                    </div>
+                </li>
+            `;
+        })
+    }
+}
+
+function novaTarefa(){
+    event.preventDefault();
+    let tarefa = {
+        titulo : titulo.value,
+        descricao : descricao.value
+    }
+    fetch("http://localhost:3000/tarefas", {
+        method : "POST",
+        headers: {
+            "Content-type" : "application/json"
+        },
+        body: JSON.stringify(tarefa)
+    })
+    .then(res => res.json())
+    .then(res => {
+        fecharModal();
+        buscarTarefas;
+    })
+}
+
+
+function deletarTarefa(id){
+    fetch(`http://localhost:3000/tarefas/${id}`,{
+        method: "DELETE",
+    })
+    .then(res => res.json())
+    .then(res => {
+        alert("ok")
+        buscarTarefas();
+    })
+    
+}
+
+
+
+function pesquisarTarefas(){
+    let lis = document.querySelectorAll("ul li");
+    if(buscarTarefas.value.length > 0){
+        lis.forEach(lis => {
+            if(!lis.children[0].innerText.includes(busca.value)){
+                lis.classList.add('oculto');
+            }else{
+                lis.classList.remove('oculto')
+            }
+        })
+    }
+
+}pesquisarTarefas();
